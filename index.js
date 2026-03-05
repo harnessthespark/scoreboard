2cde8ced854c2a84280a2a3c3a9585ab5e97d538
harnessthespark <lisa@harnessthespark.com>
2026-03-05 17:29:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let scoreHome = 0
+let scoreAway = 0
+
+function addHomeOne() {
+    scoreHome += 1
+    document.getElementById("scoreHome").textContent = scoreHome
+}
+
+function addHomeTwo() {
+    scoreHome += 2
+    document.getElementById("scoreHome").textContent = scoreHome
+}
+
+function addHomeThree() {
+    scoreHome += 3
+    document.getElementById("scoreHome").textContent = scoreHome
+}
+
+function addAwayOne() {
+    scoreAway += 1
+    document.getElementById("scoreAway").textContent = scoreAway
+}
+
+function addAwayTwo() {
+    scoreAway += 2
+    document.getElementById("scoreAway").textContent = scoreAway
+}
+
+function addAwayThree() {
+    scoreAway += 3
+    document.getElementById("scoreAway").textContent = scoreAway
+}
